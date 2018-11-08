6325c4ea9daefcca8b7a6785a583e35a88a60236
Ashley Stovall <plainsane+github@gmail.com>
2018-11-08 18:22:24 -0500

    clean up test script since python2 is in bad shaope obn my mac

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pip3 install -e .
-python3 -m coverage run --source hcl -m pytest tests
+pip install -e .
+python -m coverage run --source hcl -m pytest tests
 if [ "$?" != "0" ]; then
 	exit 1
 fi
